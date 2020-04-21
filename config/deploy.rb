274cfd7d2df18e97ad65c1c6ce5f1021229fae0d
Igor Rudym <irudym@gmail.com>
2020-04-21 17:36:26 +0300

    Capistrano update

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:irudym/iloilo-client"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/iloilo-client-test"
+set :deploy_to, "/var/www/iloilo-client"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
